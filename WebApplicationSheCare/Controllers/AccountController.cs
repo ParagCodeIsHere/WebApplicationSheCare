62d5877667730d1b0b1c6af95972712910162137
Pragyanshu Aryan <papragyanshuaryan@gmail.com>
2023-09-29 23:05:36 +0530

    register implemented through controller

--- WebApplicationSheCare/Controllers/AccountController.cs
+++ WebApplicationSheCare/Controllers/AccountController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebApplicationSheCare.Models;
 
 namespace WebApplicationSheCare.Controllers
 {
     public class AccountController : Controller
     {
+        public UserManager<IdentityUser> UserManager { get; }
+        public SignInManager<IdentityUser> SignInManager { get; }
+        public AccountController(UserManager<IdentityUser> userManager,
+
+                                SignInManager<IdentityUser> signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterView model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                var result = await UserManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }
+                foreach(var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
         }
     }
 }
